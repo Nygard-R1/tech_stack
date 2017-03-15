15b15896b5e86eec98a281ad0ef6fdc7482ae446
Nygard-R1 <thomas.sickert@gmail.com>
2017-03-14 21:00:18 -0500

    Finished Tech Stack

--- index.android.js
+++ index.android.js
@@ -1,4 +1,7 @@
-import { AppRegistry } from 'react-native';
-import App from './src/app.js';
+import { AppRegistry, UIManager } from 'react-native';
+import App from './src/App.js';
+
+// Need this in order for animations to work on android
+UIManager.setLayoutAnimationEnabledExperimental && UIManager.setLayoutAnimationEnabledExperimental(true);
 
 AppRegistry.registerComponent('tech_stack', () => App);
